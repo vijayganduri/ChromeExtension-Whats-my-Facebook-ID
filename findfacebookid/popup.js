55d0d85dab45b50bb8d49e11dd7a6ffa7c1044d2
Vijay Ganduri <vijayganduri@gmail.com>
2013-03-02 17:30:44 +0530

    icons updated, removed garbage files

--- findfacebookid/popup.js
+++ findfacebookid/popup.js
@@ -1,6 +1,4 @@
 // Copyright (c) 2013 Vijay Ganduri. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 function findID(){
 	var x = document.getElementById("fbUserNameInput").value;
@@ -18,18 +16,6 @@ function findID(){
 	document.getElementById("fbpic").innerHTML="<img src=http://graph.facebook.com/"+data.id+"/picture?width=100&height=100></img>";
   });
 
-}
-
-function show() {
-  var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
-  var hour = time[1] % 12 || 12;               // The prettyprinted hour.
-  var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
-  var notification = window.webkitNotifications.createNotification(
-    '48.png',                      // The image.
-    hour + time[2] + ' ' + period, // The title.
-    'Time to make the toast.'      // The body.
-  );
-  notification.show();
 }
 
 window.onload = function() {
